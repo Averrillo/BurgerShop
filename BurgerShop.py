c8eb0842650c3cf7d859aa50891a387065613869
Anthony Verrillo <verrillo_anthony@hotmail.ca>
2022-06-15 15:41:18 -0400

    Finished Combo class

--- BurgerShop.py
+++ BurgerShop.py
@@ -1,41 +1,74 @@
 import copy
 
+CONDIMENTS = ['ketchup', 'mustard', 'mayo', 'onion', 'tomato', 'lettuce']
+BURGER_MENU = {'original': 5.99, 'cheeseburger': 6.99, 'double cheeseburger': 8.99}
+DRINK_MENU = {'coke', 'sprite', 'rootbeer'}
+SIZE_MENU = {'small': 1.50, 'medium': 2, 'large': 3}
+SIDE_MENU = {'fries': 2, 'onion rings': 3, 'poutine': 3.50}
+
 
 class FoodItem:
-    item_name = ""
-    number = 0
-
-    def __init__(self, item_name, number):
+    def __init__(self, item_name, price):
         self.item_name = item_name
-        self.number = number
+        self.price = price
 
-    def display(self):
-        print(f"{self.item_name} added to order")
-
+    def get_price(self):
+        return self.price
 
 class Burger(FoodItem):
 
-    condiment_menu = ['ketchup', 'mustard', 'mayo', 'onion', 'tomato', 'lettuce']
-    burger_menu = {'original': 5.99, 'cheeseburger': 6.99, 'double cheeseburger': 8.99}
-
-    def __init__(self, item_name, number):
-        super().__init__(item_name, number)
+    def __init__(self, item_name, price):
+        super().__init__(item_name, price)
         self.condiment_list = []
+
+    def add_condiment(self, condiment):
+        if condiment not in self.condiment_list:
+            self.condiment_list.append(condiment)
 
     def getlist(self):
         return self.condiment_list
 
+    def display(self):
+        string = "Item: " + self.item_name + "\n" + "Price: " + str(self.price) + "$" + "\n" + "Condiments: " + ", ".join(self.condiment_list)
+        return string
+
 
 class Drink(FoodItem):
-    drink_menu = {'small': 1.50, 'medium': 2, 'large': 3}
+    def __init__(self, item_name, size, price):
+        super(Drink, self).__init__(item_name, price)
+        self.size = size
+
+    def display(self):
+        string = "Item: " + self.item_name + "\n" + "Price: " + str(self.price) + "$" + "\n" + "Size: " + self.size
+        return string
 
 
 class Side(FoodItem):
-    side_menu = {'fries': 2, 'onion rings': 3, 'poutine': 3.50}
+    def __init__(self, item_name, price):
+        super(Side, self).__init__(item_name, price)
+
+    def display(self):
+        string = "Item: " + self.item_name + "\n" + "Price: " + str(self.price) + "$"
+        return string
 
 
 class Combo(FoodItem):
-    pass
+
+    DISCOUNT = .85
+
+    def __init__(self, item_name, b, d, s, price):
+        super().__init__(item_name, price)
+        self.item_name = item_name
+        self.Burger = b
+        self.Drink = d
+        self.Side = s
+        self.price = (self.Burger.get_price() + self.Side.get_price() + self.Drink.get_price()) * self.DISCOUNT
+
+    def display(self):
+        string = "Item: " + self.item_name + "\n"
+        string = string + str(self.Burger.item_name) + "\n" + str(self.Drink.item_name) + "\n" + str(self.Side.item_name) + "\n"
+        string = string + "Combo price: " + str(self.price) + "\n"
+        return string
 
 
 class Order:
@@ -46,19 +79,39 @@ class Order:
     def __init__(self, client_name):
         self.client_name = client_name
 
-    def display_amount(self):
+    def display(self):
         for i in self.order_list:
-            print(f'You ordered {i.number} {i.item_name}')
+            print(i.display())
+
+    def check_combo(self):
+        burger_count = 0
+        side_count = 0
+        drink_count = 0
+
+        for i in self.order_list:
+            if isinstance(i, Burger):
+                burger_count += 1
+            elif isinstance(i, Side):
+                side_count += 1
+            elif isinstance(i, Drink):
+                drink_count += 1
+            else:
+                print("There are not enough items to make a combo")
+
 
 
 def user_input_burger():
 
     count = 0
     burger = input("What kind of burger would you like original, cheeseburger or double cheeseburger: ").lower()
-    number = input("How many would you like").lower()
-    b = Burger(burger, number)
 
-    updated_condiment = copy.deepcopy(b.condiment_menu)
+    if burger in BURGER_MENU:
+        price = BURGER_MENU[burger]
+        b = Burger(burger, price)
+    else:
+        print("Sorry that is not on our menu")
+
+    updated_condiment = copy.deepcopy(CONDIMENTS)
     print(f'Please choose up to three condiments: ')
 
     while count < 3:
@@ -81,22 +134,55 @@ def user_input_burger():
 
 def user_input_drink():
 
-    drink = input()
-    d = Drink()
-    # ask user for input and store it in drink object
+    print("These are the drinks available")
+    print(DRINK_MENU)
+    print("these are the available sizes and their prices")
+    print(SIZE_MENU)
+
+    while True:
+        temp = input("What drink would you want").lower()
+        if temp in DRINK_MENU:
+            drink_name = temp
+            break
+        else:
+            print("Please enter a valid drink")
+
+    while True:
+        temp = input("What size drink do you want").lower()
+        if temp in SIZE_MENU:
+            drink_size = temp
+            break
+        else:
+            print("Please enter a valid drink size")
+
+    d = Drink(drink_name, drink_size, SIZE_MENU[drink_size])
     return d
 
 
 def user_input_side():
-    s = Side()
-    # ask user for input and store it in side object
+
+    print("These are the sides available with their relative prices")
+    print(SIDE_MENU)
+
+    while True:
+        side_name = input("What side would you want").lower()
+        if side_name in SIDE_MENU:
+            break
+        else:
+            print("Please enter a valid side")
+    s = Side(side_name, SIDE_MENU[side_name])
     return s
 
 
 def user_input_combo():
-    c = Combo()
-    # ask user for input and store it in combo object
-    # a combo must include one burger, one side, and one drink
+    print("Let's order your combo!")
+    print("let's start with your burger.")
+    b = user_input_burger()
+    print("Now what would you like for a side")
+    s = user_input_side()
+    print("Lastly, what would you like for a drink")
+    d = user_input_drink()
+    c = Combo("Combo", b, d, s, 0)
     return c
 
 
@@ -131,13 +217,12 @@ def take_order():
             order.order_list.append(o)
 
         elif menu_num == '5':
-            order.display_amount()
+            order.check_combo()
+            order.display()
+
             break
 
         else:
             print("Please enter a valid number")
-
-    for i in order.order_list:
-        print(i.getlist())
 
 take_order()
